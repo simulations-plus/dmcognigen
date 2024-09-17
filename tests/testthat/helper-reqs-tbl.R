cb1e607cd404c52180bb3360e9f37c5221c2d022
Nash Delcamp <107637296+nash-delcamp-slp@users.noreply.github.com>
2024-09-17 14:46:51 -0400

    formatting

--- tests/testthat/helper-reqs-tbl.R
+++ tests/testthat/helper-reqs-tbl.R
@@ -1,4 +1,4 @@
-mk_dummy_reqs_table <- function(){
+mk_dummy_reqs_table <- function() {
   
   structure(list(path = c("../requirements_test/100103-d1pk-req-2023-01-03.xlsm", 
                           "../requirements_test/100103-d1pk-req-2023-01-18.xlsm", "../requirements_test/205024-lx9211-d1pk-req-2022-10-26.xlsm"
@@ -26,11 +26,11 @@ mk_dummy_reqs_table <- function(){
 dummy_reqs_table <- mk_dummy_reqs_table()
 
 
-make_temp_dir <- function(){
+make_temp_dir <- function() {
  
   dummy_dir <- tempdir()
   
-  if(dir.exists(dummy_dir)){
+  if(dir.exists(dummy_dir)) {
     unlink(dummy_dir,recursive = TRUE)
   }
   
@@ -44,29 +44,17 @@ dummy_names <- basename(dummy_reqs_table$path)
 
 dummy_times <- dummy_reqs_table$modified
 
-dummy_paths <- file.path(dummy_dir,dummy_names)
+dummy_paths <- file.path(dummy_dir, dummy_names)
 
-
-
-purrr::walk2(dummy_names,dummy_times,~{
+purrr::walk2(dummy_names, dummy_times, ~ {
   
   openxlsx::write.xlsx(
   list(specs = example_requirements, unit_conversions = data.frame(), discussions = data.frame()),
-  file = file.path(dummy_dir,.x)
+  file = file.path(dummy_dir, .x)
   )
   
-  Sys.setFileTime(file.path(dummy_dir,.x),.y)
+  Sys.setFileTime(file.path(dummy_dir, .x), .y)
   
   }
 
 )
-
-
-
-
-
-
-
-
-
-
